33bd2fa363ea13fd39a1b89680203c518bde3bef
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-11 23:12:44 +0700

    Membuat class untuk app

---
+++ app/src/main/java/com/den/finalproject/ui/WashXBetApp.kt
@@ -0,0 +1,13 @@
+package com.den.finalproject.ui
+
+import android.app.Application
+import com.den.finalproject.repositori.ContainerApp
+import com.den.finalproject.repositori.ContainerDataApp
+
+class WashXBetApp : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
