b55ef14d42d14f63e0f8b0f15225692c65f8b885
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-12 04:17:09 +0700

    Menambahkan class untuk OfflineRepositoriKendaraan

--- app/src/main/java/com/den/finalproject/repositori/OfflineRepositoriPelanggan.kt
+++ app/src/main/java/com/den/finalproject/repositori/OfflineRepositoriPelanggan.kt
@@ -1,14 +1,23 @@
 package com.den.finalproject.repositori
 
+import com.den.finalproject.data.Kendaraan
+import com.den.finalproject.data.KendaraanDao
 import com.den.finalproject.data.Pelanggan
 import com.den.finalproject.data.PelangganDao
 import kotlinx.coroutines.flow.Flow
 
-class OfflineRepositoriPelanggan(private val pelangganDao: PelangganDao) :
-    RepositoriPelanggan {
+class OfflineRepositoriPelanggan(private val pelangganDao: PelangganDao) : RepositoriPelanggan {
     override fun getAllPelangganStream(): Flow<List<Pelanggan>> = pelangganDao.getAllPelanggan()
     override fun getPelangganStream(id: Int): Flow<Pelanggan?> = pelangganDao.getPelanggan(id)
     override suspend fun insertPelanggan(pelanggan: Pelanggan) = pelangganDao.insert(pelanggan)
     override suspend fun deletePelanggan(pelanggan: Pelanggan) = pelangganDao.delete(pelanggan)
     override suspend fun updatePelanggan(pelanggan: Pelanggan) = pelangganDao.update(pelanggan)
+}
+
+class OfflineRepositoriKendaraan(private val kendaraanDao: KendaraanDao) : RepositoriKendaraan {
+    override fun getAllKendaraanStream(): Flow<List<Kendaraan>> = kendaraanDao.getAllKendaraan()
+    override fun getKendaraanStream(id: Int): Flow<Kendaraan?> = kendaraanDao.getKendaraan(id)
+    override suspend fun insertKendaraan(kendaraan: Kendaraan) = kendaraanDao.insert(kendaraan)
+    override suspend fun deleteKendaraan(kendaraan: Kendaraan) = kendaraanDao.delete(kendaraan)
+    override suspend fun updateKendaraan(kendaraan: Kendaraan) = kendaraanDao.update(kendaraan)
 }
