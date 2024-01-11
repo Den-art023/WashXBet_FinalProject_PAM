cd36407253998d2c1e18316453be051da07023ea
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-12 00:14:10 +0700

    Menambahkan function ItemDetails

--- app/src/main/java/com/den/finalproject/ui/halaman/HalamanDetail.kt
+++ app/src/main/java/com/den/finalproject/ui/halaman/HalamanDetail.kt
@@ -1,16 +1,72 @@
 package com.den.finalproject.ui.halaman
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.den.finalproject.R
+import com.den.finalproject.data.Pelanggan
+
+@Composable
+fun ItemDetails(
+    pelanggan: Pelanggan,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            ItemDetailsRow(
+                labelResId = R.string.nama,
+                itemDetail = pelanggan.nama, modifier = Modifier.padding(
+                    horizontal = 16.dp
+                )
+            )
+            ItemDetailsRow(
+                labelResId = R.string.telpon,
+                itemDetail = pelanggan.telpon, modifier = Modifier.padding(
+                    horizontal = 16.dp
+                )
+            )
+            ItemDetailsRow(
+                labelResId = R.string.alamat,
+                itemDetail = pelanggan.alamat,
+                modifier = Modifier.padding(
+                    horizontal = 16.dp
+                )
+            )
+            ItemDetailsRow(
+                labelResId = R.string.treatment,
+                itemDetail = pelanggan.treatment,
+                modifier = Modifier.padding(
+                    horizontal = 16.dp
+                )
+            )
+        }
+    }
+}
 
 @Composable
 private fun ItemDetailsRow(
