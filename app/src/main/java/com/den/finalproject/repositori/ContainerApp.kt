c3ec7fad3e992cc11abc7ccfe2bcc8dc2cd0e29b
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-12 04:18:42 +0700

    Menambahkan container untuk repositoriKendaraan

--- app/src/main/java/com/den/finalproject/repositori/ContainerApp.kt
+++ app/src/main/java/com/den/finalproject/repositori/ContainerApp.kt
@@ -5,10 +5,16 @@ import com.den.finalproject.data.DatabaseCustomer
 
 interface ContainerApp {
     val repositoriPelanggan: RepositoriPelanggan
+
+    val repositoriKendaraan: RepositoriKendaraan
 }
 
 class ContainerDataApp(private val context: Context) : ContainerApp {
     override val repositoriPelanggan: RepositoriPelanggan by lazy {
         OfflineRepositoriPelanggan(DatabaseCustomer.getDatabase(context).PelangganDao())
+    }
+
+    override val repositoriKendaraan: RepositoriKendaraan by lazy {
+        OfflineRepositoriKendaraan(DatabaseCustomer.getDatabase(context).KendaraanDao())
     }
 }
