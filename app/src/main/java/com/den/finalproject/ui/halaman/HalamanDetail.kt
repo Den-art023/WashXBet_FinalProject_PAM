a17d28f47f2df4afd1dec84441e356ea75231f21
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-12 00:09:56 +0700

    Menambahkan function untuk confirmasi

---
+++ app/src/main/java/com/den/finalproject/ui/halaman/HalamanDetail.kt
@@ -0,0 +1,31 @@
+package com.den.finalproject.ui.halaman
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.den.finalproject.R
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { /*DoNothing*/ },
+        title = { Text(stringResource(R.string.attention)) },
+        text = { Text(stringResource(R.string.delete)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = stringResource(R.string.cancel))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = stringResource(R.string.confirm))
+            }
+        })
+}
