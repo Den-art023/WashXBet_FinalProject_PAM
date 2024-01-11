bb7f3a6f4c35193abd8d6e0b6c4d9d7630acae8d
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-12 00:16:16 +0700

    Menambahkan function ItemDetailsBody

--- app/src/main/java/com/den/finalproject/ui/halaman/HalamanDetail.kt
+++ app/src/main/java/com/den/finalproject/ui/halaman/HalamanDetail.kt
@@ -11,15 +11,57 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.den.finalproject.R
 import com.den.finalproject.data.Pelanggan
+import com.den.finalproject.model.ItemDetailsUiState
+import com.den.finalproject.model.toPelanggan
+
+@Composable
+private fun ItemDetailsBody(
+    itemDetailsUiState: ItemDetailsUiState,
+    onDelete: () -> Unit,
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+        ItemDetails(
+            pelanggan = itemDetailsUiState.detailPelanggan.toPelanggan(),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
 
 @Composable
 fun ItemDetails(
