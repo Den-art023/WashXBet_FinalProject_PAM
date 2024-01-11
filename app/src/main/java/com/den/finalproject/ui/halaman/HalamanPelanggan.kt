6a81cee5eea50a46dda890938d69f7cc8c44fd69
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-11 23:40:59 +0700

    Menambahkan function untuk list pelanggan

--- app/src/main/java/com/den/finalproject/ui/halaman/HalamanPelanggan.kt
+++ app/src/main/java/com/den/finalproject/ui/halaman/HalamanPelanggan.kt
@@ -1,9 +1,12 @@
 package com.den.finalproject.ui.halaman
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +15,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.den.finalproject.data.Pelanggan
+
+@Composable
+fun ListPelanggan(
+    itemPelanggan: List<Pelanggan>,
+    modifier: Modifier = Modifier,
+    onItemClick: (Pelanggan) -> Unit
+) {
+    LazyColumn(modifier = Modifier) {
+        items(items = itemPelanggan, key = { it.id })
+        { buy ->
+            DataPelanggan(
+                pelanggan = buy,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable { onItemClick(buy) }
+            )
+        }
+    }
+}
 
 @Composable
 fun DataPelanggan(
