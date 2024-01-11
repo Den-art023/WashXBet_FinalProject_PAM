291764fc69e8d965035329febc6c870c89fc2ce9
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-11 23:17:38 +0700

    Membuat view model untuk pelanggan

---
+++ app/src/main/java/com/den/finalproject/model/PelangganViewModel.kt
@@ -0,0 +1,29 @@
+package com.den.finalproject.model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.den.finalproject.data.Pelanggan
+import com.den.finalproject.repositori.RepositoriPelanggan
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class PelangganViewModel(private val repositoriPelanggan: RepositoriPelanggan) : ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val pelangganUiState: StateFlow<PelangganUiState> =
+        repositoriPelanggan.getAllPelangganStream().filterNotNull()
+            .map { PelangganUiState(listPelanggan = it.toList()) }
+            .stateIn(
+                scope = viewModelScope, started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = PelangganUiState()
+            )
+
+    data class PelangganUiState(
+        val listPelanggan: List<Pelanggan> = listOf()
+    )
+}
