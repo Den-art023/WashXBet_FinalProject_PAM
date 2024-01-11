3c4cd94f31e0304108e87b2b613aff793823e40a
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-12 00:18:14 +0700

    Menambahkan function untuk screen

--- app/src/main/java/com/den/finalproject/ui/halaman/HalamanDetail.kt
+++ app/src/main/java/com/den/finalproject/ui/halaman/HalamanDetail.kt
@@ -7,26 +7,85 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.den.finalproject.R
 import com.den.finalproject.data.Pelanggan
+import com.den.finalproject.model.DetailsViewModel
 import com.den.finalproject.model.ItemDetailsUiState
+import com.den.finalproject.model.PenyediaViewModel
 import com.den.finalproject.model.toPelanggan
+import com.den.finalproject.navigasi.WashTopAppBar
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigasiBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailsViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            WashTopAppBar(
+                title = stringResource(DestinasiDetailPemesanan.titleRes),
+                canNavigateBack = true, navigateUp = navigasiBack
+            )
+        }, floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navigateToEditItem(uiState.value.detailPelanggan.id) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update)
+                )
+            }
+        }, modifier = modifier
+    ) { innerPadding ->
+        ItemDetailsBody(
+            itemDetailsUiState = uiState.value,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.deleteItem()
+                    navigasiBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
 
 @Composable
 private fun ItemDetailsBody(
