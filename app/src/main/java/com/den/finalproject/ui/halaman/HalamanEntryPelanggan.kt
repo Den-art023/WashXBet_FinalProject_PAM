d37021791b0f5b4c0c3b0a2158a463f6b620a33f
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-12 00:05:51 +0700

    Menambahkan function PelangganScreen

--- app/src/main/java/com/den/finalproject/ui/halaman/HalamanEntryPelanggan.kt
+++ app/src/main/java/com/den/finalproject/ui/halaman/HalamanEntryPelanggan.kt
@@ -15,16 +15,57 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.den.finalproject.R
 import com.den.finalproject.model.DetailPelanggan
+import com.den.finalproject.model.InputViewModel
+import com.den.finalproject.model.PenyediaViewModel
 import com.den.finalproject.model.UIStatePelanggan
+import com.den.finalproject.navigasi.WashTopAppBar
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PelangganScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InputViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            WashTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes), canNavigateBack = true,
+                navigateUp = navigateBack, scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
+        EntryPelangganBody(
+            uiStatePelanggan = viewModel.uiStatePelanggan,
+            onPelangganValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.savePelanggan()
+                    navigateBack ()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+                .verticalScroll(rememberScrollState()).fillMaxWidth(),
+        )
+    }
+}
 
 @Composable
 fun EntryPelangganBody(
