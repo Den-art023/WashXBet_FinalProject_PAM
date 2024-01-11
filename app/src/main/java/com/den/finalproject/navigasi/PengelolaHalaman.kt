739aeadea6adb6e5be10b1c90be6d6192fbe3b0f
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-12 00:35:49 +0700

    Menambahkan fun HostNavigasi

--- app/src/main/java/com/den/finalproject/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/den/finalproject/navigasi/PengelolaHalaman.kt
@@ -11,7 +11,20 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.den.finalproject.R
+import com.den.finalproject.ui.halaman.DestinasiDetailPemesanan
+import com.den.finalproject.ui.halaman.DestinasiEdit
+import com.den.finalproject.ui.halaman.DestinasiEntry
+import com.den.finalproject.ui.halaman.DestinasiHome
+import com.den.finalproject.ui.halaman.DetailsScreen
+import com.den.finalproject.ui.halaman.Homepage
+import com.den.finalproject.ui.halaman.ItemEditScreen
+import com.den.finalproject.ui.halaman.PelangganScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,5 +45,47 @@ fun WashTopAppBar(
                     )
                 }
             }
-        })
+        }
+    )
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            Homepage(navigateToRent = { navController.navigate(DestinasiPelangggan.route) })
+        }
+        composable(DestinasiPelangggan.route) {
+            PelangganScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = { navController.navigate("${DestinasiDetailPemesanan.route}/$it") })
+        }
+        composable (DestinasiEntry.route) {
+            PelangganScreen(
+                navigateBack = { navController.popBackStack() })
+        }
+        composable(DestinasiDetailPemesanan.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiDetailPemesanan.beliIdArg) {
+                type = NavType.IntType
+            }
+            )) {
+            DetailsScreen(
+                navigateToEditItem = { navController.navigate("${DestinasiEdit.route}/$it") },
+                navigasiBack = { navController.popBackStack() })
+        }
+        composable(DestinasiEdit.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            ItemEditScreen(navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.popBackStack() })
+        }
+    }
 }
