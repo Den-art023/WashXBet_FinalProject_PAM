e64fa02d9353d266c70046dcdd9a66ee053c4cae
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-11 23:51:04 +0700

    Menambahkan function WashTopAppBar untuk halaman

--- app/src/main/java/com/den/finalproject/ui/halaman/HalamanPelanggan.kt
+++ app/src/main/java/com/den/finalproject/ui/halaman/HalamanPelanggan.kt
@@ -13,19 +13,27 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.den.finalproject.R
 import com.den.finalproject.data.Pelanggan
+import com.den.finalproject.model.PelangganViewModel
+import com.den.finalproject.model.PenyediaViewModel
+
 
 @Composable
 fun BodyPelanggan(
