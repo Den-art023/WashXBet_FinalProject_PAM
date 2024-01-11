d814e599a6e76cf404c6b1f15751babaed3708b0
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-12 00:24:29 +0700

    Menambahkan function Homepage

---
+++ app/src/main/java/com/den/finalproject/ui/halaman/HalamanHome.kt
@@ -0,0 +1,86 @@
+package com.den.finalproject.ui.halaman
+
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.den.finalproject.R
+
+@Composable
+fun Homepage(
+navigateToRent: () -> Unit,) {
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        OutlinedCard(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface
+            ),
+            border = BorderStroke(1.dp, Color.Cyan),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(50.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "",
+                    Modifier.clip(
+                        RoundedCornerShape(10.dp)
+                    )
+                )
+                Text(
+                    text = "Pelanggan",
+                    color = Color.DarkGray,
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 35.sp, modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Text(
+                    text = "Kendaraan",
+                    color = Color.DarkGray,
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 55.sp, modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .weight(1f, false),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Button(
+                modifier = Modifier.weight(1f), onClick =
+                navigateToRent
+            ) {
+                Text(text = "Lanjut")
+            }
+        }
+    }
+}
