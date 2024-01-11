f8395b6fde781b8fe5e9a0a7dff7c417edbe3214
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2024-01-12 00:28:20 +0700

    Menambahkan fun ItemEditScreen

---
+++ app/src/main/java/com/den/finalproject/ui/halaman/HalamanEdit.kt
@@ -0,0 +1,44 @@
+package com.den.finalproject.ui.halaman
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.den.finalproject.model.EditViewModel
+import com.den.finalproject.model.PenyediaViewModel
+import com.den.finalproject.navigasi.WashTopAppBar
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            WashTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true, navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        EntryPelangganBody(
+            uiStatePelanggan = viewModel.pelangganUiState,
+            onPelangganValueChange = viewModel::updateUiState, onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updatePelanggan()
+                    navigateBack()
+                }
+            }, modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
